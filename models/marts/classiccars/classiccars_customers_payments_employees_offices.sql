bc63a3526f2de2483eddfaa2f5ebb4202409e73a
hitoshi matsuda <programmer060732@gmail.com>
2024-10-02 05:43:08 +0000

    【リファクタリング】コードのフォーマットを行った

--- models/marts/classiccars/classiccars_customers_payments_employees_offices.sql
+++ models/marts/classiccars/classiccars_customers_payments_employees_offices.sql
@@ -1,32 +1,27 @@
-with customers_payments_employees_source as (
-    {{ config(schema='_classiccars_L2') }}
-    select * from {{ ref('classiccars_customers_payments_employees') }}
-),
+with
+    customers_payments_employees_source as (
+        {{ config(schema="_classiccars_L2") }}
+        select *
+        from {{ ref("classiccars_customers_payments_employees") }}
+    ),
 
-offices_source as (
-    {{ config(schema='_classiccars_L2') }}
-    select * from {{ ref('classiccars_offices') }}
-),
+    offices_source as (
+        {{ config(schema="_classiccars_L2") }}
+        select *
+        from {{ ref("classiccars_offices") }}
+    ),
 
+    agg_cus_pay_emp as (
+        select officecode, sum(amount) as amount
+        from
+            (
+                select offi.officecode as officecode, emp_amo.amount as amount
+                from customers_payments_employees_source as emp_amo
+                left join offices_source as offi on emp_amo.officecode = offi.officecode
+            )
+        group by officecode
+        order by amount desc
+    )
 
-agg_cus_pay_emp as (
-    SELECT
-        officecode,
-        SUM(amount) AS amount
-    FROM (
-        SELECT
-            offi.officecode AS officecode,
-            emp_amo.amount AS amount
-        FROM
-            customers_payments_employees_source AS emp_amo
-        LEFT JOIN
-            offices_source AS offi
-        ON
-            emp_amo.officecode = offi.officecode )
-    GROUP BY
-        officecode
-    ORDER BY
-        amount DESC
-)
-
-select * from agg_cus_pay_emp
+select *
+from agg_cus_pay_emp
